9a09f863e031a88d7fb748b13b20e4ba986735e4
Juan Bautista <JBautista@ofsoptics.com>
2025-09-25 21:08:00 -0400

    Squashed 'Helpers/ScottPlotUtilities/' content from commit df32179
    git-subtree-dir: Helpers/ScottPlotUtilities
    git-subtree-split: df321797aa2818effed4efce9f5d121885e74f2f

---
+++ ScatterSeriesWrapper.cs
@@ -0,0 +1,17 @@
+﻿using ScottPlot;
+
+namespace BreakRateT1.ScottPlotUtilities
+{
+    public class ScatterSeriesWrapper
+    {
+        public IPlottable Scatter { get; set; }
+        public DateTime[] XsOriginal { get; set; } = Array.Empty<DateTime>();
+        public double[] YsOriginal { get; set; } = Array.Empty<double>();
+        public ScatterSeriesWrapper(IPlottable scatter, DateTime[] xValues, double[] yValues)
+        {
+            Scatter = scatter;
+            XsOriginal = xValues;
+            YsOriginal = yValues;
+        }
+    }
+}
