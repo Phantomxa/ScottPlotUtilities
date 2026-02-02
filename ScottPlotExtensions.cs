c123b39448bb12e16cb244c38880b5db929d996c
Phantomxa <55juan@live.com>
2026-02-02 17:25:48 -0500

    Added ThemeMinimal.

--- ScottPlotExtensions.cs
+++ ScottPlotExtensions.cs
@@ -25,5 +25,50 @@ namespace ScottPlotUtilities
             formsPlot.Plot.Legend.FontColor = Color.FromHex("#d7d7d7");
             formsPlot.Plot.Legend.OutlineColor = Color.FromHex("#d7d7d7");
         }
+        public static void ThemeMinimal(this ScottPlot.WinForms.FormsPlot formsPlot,
+        string? title = null, float? titleSize = null,
+        string? leftTitle = null, float? leftSize = null,
+        string? rightTitle = null, float? rightSize = null,
+        string? bottomTitle = null, float? bottomSize = null)
+        {
+            var plot = formsPlot.Plot;
+
+            ScottPlot.Color minimalColor = ScottPlot.Color.FromColor(System.Drawing.Color.FromArgb(235, 235, 235));
+            ScottPlot.Color axesFrameColor = ScottPlot.Color.FromColor(System.Drawing.Color.FromArgb(255, 255, 255));
+
+            plot.Axes.FrameColor(axesFrameColor);
+            plot.Grid.XAxisStyle.MajorLineStyle.Color = minimalColor;
+            plot.Grid.YAxisStyle.MajorLineStyle.Color = minimalColor;
+            plot.Grid.XAxisStyle.MajorLineStyle.Width = 1;
+
+            foreach (var yAx in plot.Axes.GetYAxes())
+            {
+                yAx.FrameLineStyle.IsVisible = true;
+                yAx.MinorTickStyle.Color = axesFrameColor;
+                yAx.MajorTickStyle.Color = axesFrameColor;
+
+                if (yAx.Edge == ScottPlot.Edge.Left && leftTitle != null)
+                {
+                    yAx.Label.Text = leftTitle;
+                    if (leftSize.HasValue) yAx.Label.FontSize = leftSize.Value;
+                }
+                else if (yAx.Edge == ScottPlot.Edge.Right && rightTitle != null)
+                {
+                    yAx.Label.Text = rightTitle;
+                    if (rightSize.HasValue) yAx.Label.FontSize = rightSize.Value;
+                }
+            }
+
+            plot.Axes.Bottom.MajorTickStyle.Color = axesFrameColor;
+            if (bottomTitle != null) plot.Axes.Bottom.Label.Text = bottomTitle;
+            if (bottomSize.HasValue) plot.Axes.Bottom.Label.FontSize = bottomSize.Value;
+
+            if (title != null)
+            {
+                plot.Title(title, titleSize ?? 16);
+            }
+
+            formsPlot.Refresh();
+        }
     }
 }
